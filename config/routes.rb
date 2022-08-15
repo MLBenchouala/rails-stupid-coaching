e470f282a6c06cb5025c982e0a73d1e02fbf0cdd
mohamed larbi benchouala <mohamedlarbi.benchouala@gmail.com>
2022-08-15 11:36:50 +0200

    stupid coaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  get 'ask', to: 'questions#ask'
   # root "articles#index"
+  get 'answer', to: 'questions#answer'
 end
